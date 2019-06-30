be4f3ccb6f72c06d72760f725dc8a111440aebe8
Ectalite <xaxa.hueber@gmail.com>
2019-06-30 18:04:07 +0200

    First Lesson

---
+++ First C Project/src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/*We always have to start somehere...*/
+//OwO I also saw that type of comment, interesting !
+int main() {
+	printf("Hello, World!\n");
+	printf("Glad to be here !\n");
+
+	return 1;
+}
