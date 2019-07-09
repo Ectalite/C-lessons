7ec93bc3d9d4c35f6fad0ee53827aa3eb3c5f311
Ectalite <Xaxa.hueber@gmail.com>
2019-07-09 13:42:10 +0200

    Testing Cmake

---
+++ build/CMakeFiles/First_C_Project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/First_C_Project.dir/main.cpp.obj"
+  "First_C_Project.exe"
+  "First_C_Project.exe.manifest"
+  "First_C_Project.pdb"
+  "libFirst_C_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/First_C_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
